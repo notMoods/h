9af61f52bca5d7326f72395a8b2d375df8dbd987
justin <tenepid@gmail.com>
2023-04-24 14:12:57 -0700

    gg

--- MainProgram.cs
+++ MainProgram.cs
@@ -44,13 +44,11 @@ namespace styla
             {6, 12, 18, 24, 30}
             */
 
-            var head = new Node(1);
-            //head.left = new Node(2);
-            //head.right = new Node(3);
-            //head.left.left = new Node(4);
-            //head.left.right = new Node(5);
-            //head.right.left = new Node(6);
-            //head.right.right = new Node(7);
+            Node head = NodeGenerator(7);
+
+            Console.WriteLine(ToText(TreeTraversal(head)));
+
+            
 
 
 
