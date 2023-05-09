9039728a351d7cbd27efbcf562609a034e419577
justin <tenepid@gmail.com>
2023-05-08 17:01:20 -0700

    g

--- classes.cs
+++ classes.cs
@@ -1,12 +1,17 @@
 namespace Roo{
 
-    class A{
+    interface IKicker{
+        public void Kick(){Console.WriteLine("IKicker's Kick");}
+    }
+
+    class A : IKicker{
         public virtual void Run() {Console.WriteLine("this is A method");}
 
         public void Hoo() {Console.WriteLine("a's hoo");}
+       public void Kick(){Console.WriteLine("A's inner kick");}
     }
 
-    class B : A{
+    class B : A, IKicker{
         public override void Run()
         {
             Console.WriteLine("this is B method");
@@ -22,5 +27,25 @@ namespace Roo{
         }
 
         public new void Foo() {Console.WriteLine("C new in this bih");}
+    }
+
+    class Testsg{
+        static void Main(){
+            IKicker h = new A();
+            h.Kick();
+
+            A gb = new A();
+            gb.Kick();
+
+            IKicker j = gb;
+            j.Kick();
+
+            IEnumerable<object> loo = new List<object>();
+            IEnumerable<object> looh = new List<List<int>>();
+
+            IList<IList<int>> glow = new List<IList<int>>();
+
+            
+        }
     }
 }
